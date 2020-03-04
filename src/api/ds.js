7c72ab721f3c972c5f23dc4b8ad27eac691553ed
Yunwei Li <liyunwei10@gmail.com>
2020-03-04 15:09:36 -0500

    new dataset

--- src/api/ds.js
+++ src/api/ds.js
@@ -2,4 +2,5 @@ export default [
     "https://www.quandl.com/api/v3/datasets/FRED/TOTALSA.json?api_key=WKqjLz_tkGH_KqVGPq9s",
     "https://www.quandl.com/api/v3/datasets/FRED/RSXFS.json?api_key=WKqjLz_tkGH_KqVGPq9s",
     "https://www.quandl.com/api/v3/datasets/UMICH/SOC1.json?api_key=WKqjLz_tkGH_KqVGPq9s",
+    "https://www.quandl.com/api/v3/datasets/ODA/USA_LUR.json?api_key=WKqjLz_tkGH_KqVGPq9s",
 ];
